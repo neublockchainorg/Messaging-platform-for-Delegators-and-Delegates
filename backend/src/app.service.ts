3e28eb74157950705913204d5ddf8586edab2c2b
Macbeth98 <manichandra.teja@gmail.com>
2023-12-25 11:16:00 -0500

    chore:  hello message

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello There! I am a DAO Relations API server built with NestJS, GraphQL, and MongoDB.';
   }
 }
